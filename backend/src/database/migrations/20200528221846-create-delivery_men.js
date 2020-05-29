729ba61047dc92d10239fab567b989368a2352a2
zTaaso <ztaaso.cedaspy@gmail.com>
2020-05-29 00:22:23 -0300

    DeliveryMan table added

---
+++ backend/src/database/migrations/20200528221846-create-delivery_men.js
@@ -0,0 +1,46 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable(
+            'delivery_men',
+            {
+                id: {
+                    type: Sequelize.INTEGER,
+                    primaryKey: true,
+                    allowNull: false,
+                    autoIncrement: true,
+                },
+                name: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                email: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                avatar_id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: true,
+                    reference: { model: 'files', key: 'id' },
+                    onUpdate: 'CASCADE',
+                    onDelete: 'SET NULL',
+                },
+                created_at: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                },
+                updated_at: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                },
+            },
+            {
+                freezeTableName: true,
+                tableName: 'delivery_men',
+            }
+        );
+    },
+
+    down: (queryInterface) => {
+        return queryInterface.dropTable('delivery_men');
+    },
+};
