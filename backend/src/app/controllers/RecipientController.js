6269da050f67e2bcfaa94bec2ca22666be94d639
zTaaso <ztaaso.cedaspy@gmail.com>
2020-06-21 22:16:48 -0300

    show method added

--- backend/src/app/controllers/RecipientController.js
+++ backend/src/app/controllers/RecipientController.js
@@ -1,7 +1,25 @@
+import { Op } from 'sequelize';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
     async index(req, res) {
+        const { q: query } = req.query;
+
+        if (query) {
+            const recipients = await Recipient.findAll({
+                attributes: {
+                    exclude: ['updatedAt', 'createdAt'],
+                },
+                where: {
+                    name: {
+                        [Op.substring]: query,
+                    },
+                },
+            });
+
+            return res.json(recipients);
+        }
+
         const recipients = await Recipient.findAll({
             attributes: {
                 exclude: ['updatedAt', 'createdAt'],
@@ -29,6 +47,27 @@ class RecipientController {
         const { id: recipient_id, name } = recipient;
 
         return res.json({ id: recipient_id, name });
+    }
+
+    async update(req, res) {
+        const { id } = req.params;
+
+        const recipient = await Recipient.findByPk(id);
+
+        await recipient.update(req.body);
+        return res.json(recipient);
+    }
+
+    async show(req, res) {
+        const { id } = req.params;
+
+        const recipient = await Recipient.findByPk(id);
+
+        if (!recipient) {
+            return res.status(401).json({ error: 'Recipient not found.' });
+        }
+
+        return res.json(recipient);
     }
 }
 
