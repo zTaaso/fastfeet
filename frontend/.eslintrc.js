d1774c238e3d0b0547061ea2383b16139572a12f
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-18 14:22:21 -0300

    label rule disabled

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -31,5 +31,6 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/jsx-props-no-spreading': 'off',
+    'jsx-a11y/label-has-for': 'off',
   },
 };
