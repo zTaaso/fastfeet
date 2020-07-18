6dc934ecbf41000bafa8d8db71a6267f390eb4fc
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-18 14:25:16 -0300

    sign in page created

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
+import logo from '../../assets/logo.png';
+
+import { LogoContent } from './styles';
+
 function SignIn() {
-  return <h1>SignIn</h1>;
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <>
+      <header>
+        <LogoContent>
+          <img src={logo} alt="Fastfeet" />
+          {/* <h1>FastFeet</h1> */}
+        </LogoContent>
+      </header>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          Seu e-mail
+          <input type="email" id="email" placeholder="example@email.com" />
+        </label>
+
+        <label htmlFor="password">
+          Sua senha
+          <input type="password" id="password" placeholder="*********" />
+        </label>
+
+        <button type="submit">Entrar no sistema</button>
+      </form>
+    </>
+  );
 }
 
 export default SignIn;
---
+++ frontend/src/pages/SignIn/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const LogoContent = styled.div`
+  display: flex;
+  justify-content: center;
+
+  img {
+    width: 254px;
+    height: 44px;
+  }
+`;
