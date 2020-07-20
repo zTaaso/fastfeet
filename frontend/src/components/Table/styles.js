ba61ecaf0b391eab5e3e60649f269b8f5e865636
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-20 17:02:18 -0300

    added scrolling to table

--- frontend/src/components/Table/styles.js
+++ frontend/src/components/Table/styles.js
@@ -3,7 +3,17 @@ import styled from 'styled-components';
 export const Table = styled.div`
   overflow-y: scroll;
   padding: 10px 20px;
+  max-height: 400px;
+
   scrollbar-color: #eee;
+  /* Hide scrollbar for IE, Edge and Firefox */
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  &::-webkit-scrollbar {
+    display: none;
+  }
 
   table {
     width: 100%;
@@ -16,7 +26,15 @@ export const Table = styled.div`
       text-align: left;
       padding: 12px 20px;
 
+      &:last-child {
+        text-align: center;
+      }
+
       /* margin-left: 5px; */
+    }
+
+    tbody {
+      border: 1px solid black;
     }
 
     tbody tr {
@@ -24,11 +42,29 @@ export const Table = styled.div`
       border-radius: 9px;
 
       td {
-        padding: 12px 20px;
+        padding: 5px 20px 10px;
         color: #666666;
         font-size: 16px;
         height: 20px;
         text-align: left;
+        vertical-align: baseline;
+
+        label {
+          padding-bottom: 5px;
+        }
+
+        img {
+          width: 28px;
+          height: 28px;
+          position: relative;
+          top: 5px;
+          border-radius: 50%;
+          margin-right: 5px;
+        }
+      }
+
+      td:last-child {
+        text-align: center;
       }
     }
   }
