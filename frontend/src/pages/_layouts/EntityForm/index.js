d5951cfb9ba7d3477ad0192fedaa98e24de19c85
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-23 21:20:49 -0300

    goBack added

--- frontend/src/pages/_layouts/EntityForm/index.js
+++ frontend/src/pages/_layouts/EntityForm/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { IoIosArrowBack } from 'react-icons/io';
 import { MdDone } from 'react-icons/md';
 
+import { useHistory } from 'react-router-dom';
 import { Container, Content } from './styles';
 
 function EntityForm({ title, children }) {
+  const history = useHistory();
+
+  function handleGoBack() {
+    history.goBack();
+  }
+
   return (
     <Container>
       <section>
         <h1>{title}</h1>
 
         <div>
-          <button id="back" type="button">
+          <button id="back" type="button" onClick={handleGoBack}>
             <IoIosArrowBack size={20} color="#fff" />
             Voltar
           </button>
