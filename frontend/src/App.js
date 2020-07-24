7dad0b0eccc4aef7b9cb80548f93bc010c446b2d
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-24 17:32:12 -0300

    store provider added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Routes from './Routes';
 import GlobalStyles from './styles/global';
 
+import './config/reactotron';
+
+import { store } from './store';
+
 import history from './services/history';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyles />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyles />
+      </Router>
+    </Provider>
   );
 }
 
