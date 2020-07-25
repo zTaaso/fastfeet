bb00b46c12baee662278037a935c7520b1651135
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-25 17:50:07 -0300

    loading animation added

--- frontend/src/pages/SignIn/styles.js
+++ frontend/src/pages/SignIn/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const LogoContent = styled.div`
   display: flex;
@@ -7,5 +7,19 @@ export const LogoContent = styled.div`
   img {
     width: 254px;
     height: 44px;
+  }
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  } to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Button = styled.button`
+  svg {
+    animation: ${rotate} 1s linear infinite;
   }
 `;
