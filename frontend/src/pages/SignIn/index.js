93110a7481ac8e6e39f2c47093c50ddde5e9c982
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-25 17:40:11 -0300

    signIn request added

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 
 import logo from '../../assets/logo.png';
 
 import Input from '../../components/Input';
 
-import { LogoContent } from './styles';
+import { LogoContent, Button } from './styles';
+
+import { signInRequest } from '../../store/modules/auth/actions';
 
 function SignIn() {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.auth.loading);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   function handleSubmit(e) {
     e.preventDefault();
+    dispatch(signInRequest(email, password));
   }
 
   return (
@@ -27,6 +38,8 @@ function SignIn() {
           placeholder="example@email.com"
           typeName="label"
           label="Seu email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <Input
@@ -35,9 +48,17 @@ function SignIn() {
           placeholder="**********"
           typeName="label"
           label="Sua senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <button type="submit">Entrar no sistema</button>
+        <Button type="submit">
+          {loading ? (
+            <AiOutlineLoading3Quarters size={22} color="#fff" />
+          ) : (
+            'Entrar no sistema'
+          )}
+        </Button>
       </form>
     </>
   );
