02e512ac306df0ac6e0bf279f415459442399889
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-26 23:09:15 -0300

    api integrated

--- frontend/src/pages/DeliveryMenForm/index.js
+++ frontend/src/pages/DeliveryMenForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { InputStyled } from './styles';
@@ -6,18 +6,44 @@ import { InputStyled } from './styles';
 import EntityForm from '../_layouts/EntityForm';
 
 import Dropzone from '../../components/Dropzone';
+import api from '../../services/api';
 
 function DeliveryMenForm() {
-  const [imgFile, setImgFile] = useState(new File());
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const [imgFile, setImgFile] = useState(
+    new File([{ type: 'img' }], 'null.png')
+  );
+
   const img = '';
 
-  const { location } = useHistory();
-  const titleWord = location.pathname.includes('register')
+  const history = useHistory();
+  const titleWord = history.location.pathname.includes('register')
     ? 'Cadastro'
     : 'Edição';
 
+  async function handleSave() {
+    try {
+      let avatar_id = null;
+      if (imgFile.size > 20) {
+        const fileData = new FormData();
+        fileData.append('file', imgFile);
+        const response = await api.post('/files', fileData);
+        avatar_id = response.data.id;
+      }
+
+      await api.post('/deliveryman', { name, email, avatar_id });
+
+      alert('Entregador criado com sucesso!');
+      history.goBack();
+    } catch (err) {
+      alert('Algo deu errado!');
+    }
+  }
+
   return (
-    <EntityForm title={`${titleWord} de entregadores`}>
+    <EntityForm title={`${titleWord} de entregadores`} onSave={handleSave}>
       <Dropzone
         img={img}
         onFileChange={(file) => {
@@ -25,12 +51,19 @@ function DeliveryMenForm() {
         }}
       />
 
-      <InputStyled typeName="label" label="Nome" type="text" width={100} />
+      <InputStyled
+        typeName="label"
+        label="Nome"
+        type="text"
+        width={100}
+        onChange={(e) => setName(e.target.value)}
+      />
       <InputStyled
         typeName="label"
         label="Email"
         type="email"
         labelWidth={100}
+        onChange={(e) => setEmail(e.target.value)}
       />
     </EntityForm>
   );
