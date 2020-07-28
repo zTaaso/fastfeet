3117a9fe756b5f03d2ff33489b47fc3bf61c2475
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-28 12:49:20 -0300

    get values by id added

--- frontend/src/pages/Deliveries/DialogContent/index.js
+++ frontend/src/pages/Deliveries/DialogContent/index.js
@@ -1,41 +1,107 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import { FaRegSadTear } from 'react-icons/fa';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+
+import api from '../../../services/api';
 
 import { DialogContentStyled } from './styles';
 
-function DialogContent() {
+function DialogContent({ id }) {
+  const [delivery, setDelivery] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [formatedDates, setFormatedDates] = useState({});
+
+  const hasSignature = false;
+
+  useEffect(() => {
+    async function getDelivery() {
+      const response = await api.get(`delivery/${id}`);
+
+      const formatingDates = {
+        created: format(parseISO(response.data.createdAt), 'Pp', {
+          locale: pt,
+        }),
+        retired:
+          (response.data.start_date &&
+            format(parseISO(response.data.start_date), 'Pp', {
+              locale: pt,
+            })) ||
+          'Pendente',
+        delivered:
+          (response.data.end_date &&
+            format(parseISO(response.data.end_date), 'Pp', {
+              locale: pt,
+            })) ||
+          'Pendente',
+      };
+
+      setDelivery(response.data);
+      setFormatedDates(formatingDates);
+      setLoading(false);
+      console.log({ id, response: response.data });
+    }
+    getDelivery();
+  }, []);
+
   return (
     <DialogContentStyled>
-      <div>
-        <h3>Endereço</h3>
-        <p>Rua Jusciliana Narciso Campos</p>
-        <p>Ibirité - Minas Gerais</p>
-        <p>34550-3443</p>
-      </div>
-
-      <hr />
-
-      <div>
-        <h3>Datas</h3>
-        <p>
-          <strong>Retirada:</strong> 27/02/2003
-        </p>
-        <p>
-          <strong>Entrega:</strong> 27/02/2003
-        </p>
-      </div>
-
-      <hr />
-
-      <div>
-        <h3>Assinatura do destinatário</h3>
-
-        <div>
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Lily_Collins%27_Signature.png"
-            alt="Signature"
-          />
+      {loading ? (
+        <div className="loading">
+          <AiOutlineLoading3Quarters color="#000" size={30} />
         </div>
-      </div>
+      ) : (
+        <>
+          <div className="product">
+            <strong>Produto: </strong> {delivery.product}
+          </div>
+
+          <hr />
+
+          <div>
+            <h3>Endereço</h3>
+            <p>{delivery.recipient.street}</p>
+            <p>{`${delivery.recipient.city} -  ${delivery.recipient.state}`}</p>
+            <p>{delivery.recipient.zip}</p>
+          </div>
+
+          <hr />
+
+          <div>
+            <h3>Datas</h3>
+            <p>
+              <strong>Criado em:</strong> {formatedDates.created}
+            </p>
+            <p>
+              <strong>Retirada:</strong> {formatedDates.retired}
+            </p>
+            <p>
+              <strong>Entrega:</strong> {formatedDates.delivered}
+            </p>
+          </div>
+
+          <hr />
+
+          <div>
+            <h3>Assinatura do destinatário</h3>
+
+            <div>
+              {hasSignature ? (
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Lily_Collins%27_Signature.png"
+                  alt="Signature"
+                />
+              ) : (
+                <>
+                  <h4> Não há assinatura para esta encomenda </h4>
+                  <FaRegSadTear color="#000" size={30} />
+                </>
+              )}
+            </div>
+          </div>
+        </>
+      )}
     </DialogContentStyled>
   );
 }
