787369c7039a923cd0937e2d38ec2d4463b837bd
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-28 12:50:03 -0300

    button row styled added

--- frontend/src/pages/DeliveriesForm/styles.js
+++ frontend/src/pages/DeliveriesForm/styles.js
@@ -13,3 +13,38 @@ export const InputRow = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
+export const ButtonRow = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background: #de3b3b;
+
+    padding: 7px 15px;
+    border: 0;
+    border-radius: 5px;
+
+    text-transform: uppercase;
+    font-family: 'Roboto';
+    font-weight: bold;
+    color: #fff;
+
+    margin: 0 5px;
+
+    transition: 0.5s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+
+    svg {
+      margin-left: 5px;
+    }
+  }
+`;
