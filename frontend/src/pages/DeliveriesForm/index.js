df680fda736016da3ef7ab356db004d1742437c2
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-28 12:50:40 -0300

    get default values added

--- frontend/src/pages/DeliveriesForm/index.js
+++ frontend/src/pages/DeliveriesForm/index.js
@@ -1,39 +1,61 @@
 import React, { useState, useEffect } from 'react';
-
+import Proptypes from 'prop-types';
+import { toast } from 'react-toastify';
+import { AiOutlineClose } from 'react-icons/ai';
 import { useHistory } from 'react-router-dom';
-import { InputStyled, InputRow } from './styles';
+
+import { InputStyled, InputRow, ButtonRow } from './styles';
 
 import EntityForm from '../_layouts/EntityForm';
 
 import api from '../../services/api';
 
-function DeliveriesSection() {
+function DeliveriesSection({ match }) {
   const history = useHistory();
+  const deliveryId = match.params.id;
 
   const [recipients, setRecipients] = useState([]);
-  const [selectedRecipient, setSelectedRecipient] = useState(0);
+  const [selectedRecipient, setSelectedRecipient] = useState({});
   const [deliverymen, setDeliverymen] = useState([]);
-  const [selectedDeliveryman, setSelectedDeliveryman] = useState(0);
+  const [selectedDeliveryman, setSelectedDeliveryman] = useState({});
 
   const [product, setProduct] = useState('');
 
   const [options, setOptions] = useState({ recipients: [], deliverymen: [] });
 
-  const titleWord = history.location.pathname.includes('register')
+  const status = history.location.pathname.includes('register')
     ? 'Cadastro'
     : 'Edição';
 
   async function handleSave() {
     try {
-      await api.post('/delivery', {
-        recipient_id: selectedRecipient,
-        deliveryman_id: selectedDeliveryman,
-        product,
-      });
-      alert('Encomenda criada com sucesso!');
+      if (status === 'Cadastro') {
+        await api.post('/delivery', {
+          recipient_id: selectedRecipient.value,
+          deliveryman_id: selectedDeliveryman.value,
+          product,
+        });
+      } else {
+        await api.put(`/delivery/${deliveryId}`, {
+          recipient_id: selectedRecipient.value,
+          deliveryman_id: selectedDeliveryman.value,
+          product,
+        });
+      }
+
+      toast.success('Encomenda salva com sucesso!');
       history.goBack();
     } catch (err) {
-      alert('Algo deu errado na criação da entrega.');
+      toast.error('Algo deu errado na criação da entrega.');
+    }
+  }
+
+  async function handleCancel() {
+    const confirmation = window.confirm('Cancelar encomenda?');
+    if (confirmation) {
+      await api.delete(`delivery/${deliveryId}`);
+      toast.success('Encomenda cancelada com sucesso!');
+      history.goBack();
     }
   }
 
@@ -63,6 +85,26 @@ function DeliveriesSection() {
   }, []);
 
   useEffect(() => {
+    async function getDelivery(id) {
+      const response = await api.get(`delivery/${id}`);
+      const delivery = response.data;
+      setSelectedDeliveryman({
+        label: delivery.deliveryman.name,
+        value: delivery.deliveryman.id,
+      });
+      setSelectedRecipient({
+        label: delivery.recipient.name,
+        value: delivery.recipient.id,
+      });
+      setProduct(delivery.product);
+    }
+
+    if (status === 'Edição') {
+      getDelivery(deliveryId);
+    }
+  }, []);
+
+  useEffect(() => {
     const serializedOptions = {
       recipients: recipients.map((recipient) => ({
         label: recipient.name,
@@ -78,7 +120,7 @@ function DeliveriesSection() {
   }, [recipients, deliverymen]);
 
   return (
-    <EntityForm title={`${titleWord} de encomendas`} onSave={handleSave}>
+    <EntityForm title={`${status} de encomendas`} onSave={handleSave}>
       <InputRow>
         <InputStyled
           typeName="labeled-select"
@@ -86,7 +128,8 @@ function DeliveriesSection() {
           type="text"
           width={95}
           options={options.recipients}
-          onChange={(e) => setSelectedRecipient(e.value)}
+          value={selectedRecipient}
+          onChange={(e) => setSelectedRecipient(e)}
         />
         <InputStyled
           typeName="labeled-select"
@@ -94,7 +137,8 @@ function DeliveriesSection() {
           type="text"
           width={100}
           options={options.deliverymen}
-          onChange={(e) => setSelectedDeliveryman(e.value)}
+          value={selectedDeliveryman}
+          onChange={(e) => setSelectedDeliveryman(e)}
         />
       </InputRow>
 
@@ -106,8 +150,29 @@ function DeliveriesSection() {
         value={product}
         onChange={(e) => setProduct(e.target.value)}
       />
+
+      {status === 'Edição' && (
+        <ButtonRow>
+          <button type="button" onClick={handleCancel}>
+            Cancelar encomenda
+            <AiOutlineClose color="#fff" size={20} />
+          </button>
+        </ButtonRow>
+      )}
     </EntityForm>
   );
 }
 
 export default DeliveriesSection;
+
+DeliveriesSection.propTypes = {
+  match: Proptypes.shape({
+    params: { id: Proptypes.number },
+  }),
+};
+
+DeliveriesSection.defaultProps = {
+  match: {
+    params: { id: null },
+  },
+};
