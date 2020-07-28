8acc2435c2aecb46b7dff5b0d92581f30d9a0457
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-28 12:49:49 -0300

    product row styled added

--- frontend/src/pages/Deliveries/DialogContent/styles.js
+++ frontend/src/pages/Deliveries/DialogContent/styles.js
@@ -1,9 +1,36 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+  from {
+    transform: rotate(360deg);
+  } to {
+    transform: rotate(0deg);
+  }
+`;
 
 export const DialogContentStyled = styled.div`
   hr {
     margin: 10px 1px;
     opacity: 0.5;
+  }
+
+  div.loading {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    svg {
+      animation: ${rotate} 1s linear infinite;
+    }
+  }
+
+  div.product {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    strong {
+      margin-right: 6px;
+    }
   }
 
   div {
@@ -19,6 +46,10 @@ export const DialogContentStyled = styled.div`
     img {
       max-width: 300px;
       max-height: 80px;
+    }
+
+    svg {
+      margin-top: 5px;
     }
   }
 `;
