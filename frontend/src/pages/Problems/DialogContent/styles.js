5d41437a4171579fe30a0f72c4cddf8b3783e3ed
zTaaso <ztaaso.cedaspy@gmail.com>
2020-07-30 00:26:05 -0300

    unused tyles removed

--- frontend/src/pages/Problems/DialogContent/styles.js
+++ frontend/src/pages/Problems/DialogContent/styles.js
@@ -1,30 +1,14 @@
 import styled from 'styled-components';
 
 export const DialogContentStyled = styled.div`
-  hr {
-    margin: 10px 1px;
-    opacity: 0.5;
+  div.loading {
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   div {
-    h3 {
-      margin-bottom: 10px;
-    }
+    overflow-wrap: break-word;
   }
-
-  div div {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-
-    img {
-      max-width: 250px;
-      max-height: 500px;
-      border-radius: 15%;
-    }
-
-    h2 {
-      margin: 10px 0;
-    }
   }
 `;
